6e059afd74f2951b6c8b6a8e8a74d500a584ba28
Atasha Potter <Ms.APotter@gmail.com>
2019-04-15 15:51:37 -0400

    stuck in infinite loop

--- main.js
+++ main.js
@@ -74,55 +74,63 @@ shuffleArray(fullDeck)
 /**************************************************/
 
 playerOneCards = fullDeck.splice(0,26)
-// console.log(playerOneCards)
-// console.log(playerOneCards.length)
-// console.log(playerOneCards[0].score)
 playerTwoCards = fullDeck.splice(0,26)
-// console.log(playerTwoCards)
-// console.log(playerTwoCards[0].score)
-// console.log(playerTwoCards.length)
 
 
 /*********** 5. handle the logic for adding the cards to bottom of the stack of the winning player.  ***********/
 /**************************************************/
 
 
-x = 0;
-    while (playerOneCards.length !== 52 || playerTwoCards.length !== 52) {    // Step 7. Once one player has all 52 cards in their stack....
-        
-        if (playerOneCards[x].score > playerTwoCards[x].score) {
-            console.log("Player One wins")
-            playerOneCards.push(playerTwoCards[x]);
-            playerTwoCards.pop();
+// x = 0; // ==> CANT USE X AND INDECES BC AT SOME POINT THE INDEX WILL BE LARGER THAN WHAT IS IN THE ARRAY, RESULTING IN AN ERROR MSG
 
-            playerOneCards.push(playerOneCards[x]);     
-            playerOneCards.shift()
-            x++;
 
-                    if (playerOneCards.length === 52 ) {
-                        console.log("Game over. Player One is the winner!")
-                    }
+function playGame() {
+    
+        while (playerOneCards.length !== 51 || playerTwoCards.length !== 51) {    // Step 7. Once one player has all 52 cards in their stack....
+                
+                if (playerOneCards[0].score > playerTwoCards[0].score) {
+                    console.log("Player One wins")
+                    playerOneCards.push(playerTwoCards[0]);     // Pushes top card of P2 to end of Player 1 array
+                    playerTwoCards.shift();                     // Removes P2 top card from P2 array
 
-        } else if (playerTwoCards[x].score > playerOneCards[x].score) {
-            console.log("Player Two wins")
-            playerTwoCards.push(playerOneCards[x]);
-            playerOneCards.pop();
-            playerTwoCards.push(playerTwoCards[x]);
-            playerTwoCards.shift()
-            x++;              
-            
-                    if (playerTwoCards.length === 52 ) {
-                        console.log("Game over. Player Two is the winner!")
-                    }
-            
-        } else if (playerOneCards[x].score == playerTwoCards[x].score) {    // Step 6. In the case of the tie, each player takes the card they played and places it 
-            console.log("It's a tie")
-            playerOneCards.push(playerOneCards[x])                        //  at the bottom of their stack, and then the game automatically plays the next round.
-            playerOneCards.shift()
-            playerTwoCards.push(playerTwoCards[x])    
-            playerTwoCards.shift()
-            x++;
-        } 
+                    playerOneCards.push(playerOneCards[0]);     // Pushes top card of P1 to end of P1 deck
+                    playerOneCards.shift()                      // Removes that top card card from P1 array
 
+                    console.log(playerOneCards.length)
+                    console.log(playerTwoCards.length)
+
+                } else if (playerTwoCards[0].score > playerOneCards[0].score) {
+                    console.log("Player Two wins")
+                    playerTwoCards.push(playerOneCards[0]);
+                    playerOneCards.shift();
+
+                    playerTwoCards.push(playerTwoCards[0]);
+                    playerTwoCards.shift()
+
+                    console.log(playerOneCards.length)
+                    console.log(playerTwoCards.length)
+                    
+                } else if (playerOneCards[0].score == playerTwoCards[0].score) {    // Step 6. In the case of the tie, each player takes the card they played and places it 
+                    console.log("It's a tie")
+                    playerOneCards.push(playerOneCards[0])                        //  at the bottom of their stack, and then the game automatically plays the next round.
+                    playerOneCards.shift()
+                    playerTwoCards.push(playerTwoCards[0])    
+                    playerTwoCards.shift()
+
+                    console.log(playerOneCards.length)
+                    console.log(playerTwoCards.length)
+                } 
+
+            }
+
+    if (playerOneCards.length === 51 ) {
+        return "Game over. Player One is the winner!"
+    } else if (playerTwoCards.length === 51 ) {
+        return "Game over. Player Two is the winner!"
+    } else if ( playerOneCards.length === 50 && (playerOneCards.score === playerTwoCards.score) || playerTwoCards.length === 50 && (playerOneCards.score === playerTwoCards.score) ){
+        return "Its a tie"
     }
+    
+}
+playGame()
 
